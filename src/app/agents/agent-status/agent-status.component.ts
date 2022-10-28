123e3338d44921aff750f75ffd0e8ad8bd9ff2fc
Benito Díaz-Medina <benitodm90@gmail.com>
2022-10-28 08:49:11 +0100

    css animation

--- src/app/agents/agent-status/agent-status.component.ts
+++ src/app/agents/agent-status/agent-status.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { faDigitalTachograph, faMicrochip, faHomeAlt, faPlus, faUserSecret } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-agent-status',
   templateUrl: './agent-status.component.html'
 })
 export class AgentStatusComponent implements OnInit {
+  public isCollapsed = true;
+  faDigitalTachograph=faDigitalTachograph;
+  faMicrochip=faMicrochip;
+  faHomeAlt=faHomeAlt;
+  faPlus=faPlus;
+  faUserSecret=faUserSecret;
 
   constructor() { }
 
