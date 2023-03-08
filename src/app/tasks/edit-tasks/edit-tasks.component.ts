36e908e9d0759b8bfb814391ef914d9243ef53fd
Benito Díaz-Medina <benitodm90@gmail.com>
2023-03-08 11:23:10 +0000

    Added keyspace searched calculation

--- src/app/tasks/edit-tasks/edit-tasks.component.ts
+++ src/app/tasks/edit-tasks/edit-tasks.component.ts
@@ -10,6 +10,7 @@ import { Observable, Subject } from 'rxjs';
 import { ChunkService } from '../../core/_services/chunks.service';
 import { TasksService } from '../../core/_services/tasks/tasks.sevice';
 import { AgentsService } from '../../core/_services/agents/agents.service';
+import { CrackerService } from '../../core/_services/config/cracker.service';
 import { PendingChangesGuard } from 'src/app/core/_guards/pendingchanges.guard';
 import { UIConfigService } from 'src/app/core/_services/shared/storage.service';
 
@@ -28,6 +29,7 @@ export class EditTasksComponent implements OnInit,PendingChangesGuard {
   isLoading = false;
 
   constructor(
+    private crackerService: CrackerService,
     private agentsService: AgentsService,
     private tasksService: TasksService,
     private chunkService: ChunkService,
@@ -46,7 +48,7 @@ export class EditTasksComponent implements OnInit,PendingChangesGuard {
   dtTrigger: Subject<any> = new Subject<any>();
   dtOptions: any = {};
   uidateformat:any;
-
+  crackerinfo:any;
   getchunks: any;
 
   ngOnInit() {
@@ -129,10 +131,15 @@ export class EditTasksComponent implements OnInit,PendingChangesGuard {
     if (this.editMode) {
     this.tasksService.getTask(this.editedTaskIndex).subscribe((result)=>{
       this.color = result['color'];
+      this.crackerService.getCrackerBinary(result['crackerBinaryId']).subscribe((val) => {
+        this.crackerinfo = val;
+      });
+      this.tkeyspace = result['keyspace'];
+      this.tusepreprocessor = result['usePreprocessor'];
       this.updateForm = new FormGroup({
         'taskId': new FormControl(result['taskId']),
         'forcePipe': new FormControl(result['forcePipe']== true? 'Yes':'No'),
-        'skipKeyspace': new FormControl(result['skipKeyspace']),
+        'skipKeyspace': new FormControl(result['skipKeyspace'] > 0?result['skipKeyspace']:'N/A'),
         'keyspace': new FormControl(result['keyspace']),
         'keyspaceProgress': new FormControl(result['keyspaceProgress']),
         'crackerBinaryId': new FormControl(result['crackerBinaryId']),
@@ -191,17 +198,30 @@ export class EditTasksComponent implements OnInit,PendingChangesGuard {
     }
   }
 
+  // Calculations to get Keyspace searched,
+  cprogress: any;
+  tkeyspace: any;
+  tusepreprocessor: any;
+  timeCalc(chunks){
+      var cprogress = []
+      for(let i=0; i < chunks.length; i++){
+        cprogress.push(chunks[i].checkpoint - chunks[i].skip);
+      }
+      this.cprogress = cprogress.reduce((a, i) => a + i);
+  }
+
   assignChunksInit(id: number){
     let params = {'maxResults': this.maxResults};
     this.chunkService.getChunks(params).subscribe((result: any)=>{
       var getchunks = result.values.filter(u=> u.taskId == id);
+      this.timeCalc(getchunks);
       this.agentsService.getAgents(params).subscribe((agents: any) => {
       this.getchunks = getchunks.map(mainObject => {
         let matchObject = agents.values.find(element => element.agentId === mainObject.agentId)
         return { ...mainObject, ...matchObject }
-      })
+        })
       this.dtTrigger.next(void 0);
-    });
+      });
     });
 
     this.dtOptions[2] = {
