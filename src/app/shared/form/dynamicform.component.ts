e8408056c14c0610ca511760479b8ff22e16d6f3
Benito Díaz-Medina <benitodm90@gmail.com>
2023-11-07 23:14:12 +0000

    value zero for files

--- src/app/shared/form/dynamicform.component.ts
+++ src/app/shared/form/dynamicform.component.ts
@@ -16,70 +16,78 @@ import { ChangeDetectorRef } from '@angular/core';
 <app-page-subtitle [subtitle]="subtitle"></app-page-subtitle>
 <grid-main [class]="'width:100%;'" [centered]="true">
   <form [formGroup]="form" (ngSubmit)="onSubmit()">
-    <div *ngFor="let field of formMetadata">
-    <div class="form-group" *ngIf="field.type !== 'hidden'">
-      <ng-container *ngIf="field.isTitle">
-        <h5>{{ field.label }}</h5>
-      </ng-container>
-      <ng-container *ngIf="!field.isTitle">
-        <div class="form-group">
-          <div class="form-outline form-input-custom">
-            <label [class.requiredak]="field.requiredasterisk" class="form-label" [for]="field.name">{{ field.label }}</label>
-            <fa-icon
-                  placement="bottom"
-                  ngbTooltip="{{field.tooltip}}"
-                  container="body"
-                  [icon]="faInfoCircle"
-                  aria-hidden="true"
-                  class="gray-light-ico display-col"
-                  *ngIf="field.tooltip"
-            ></fa-icon>
-            <ng-container [ngSwitch]="field.type">
-              <td *ngSwitchCase="'number'">
-                <input class="form-control" type="number" [formControlName]="field.name">
-              </td>
-              <div *ngSwitchCase="'text'">
-                <input class="form-control" [type]="field.type" [formControlName]="field.name">
-              </div>
-              <div *ngSwitchCase="'password'">
-                <input class="form-control" type="password" [formControlName]="field.name">
-              </div>
-              <div *ngSwitchCase="'textarea'">
-                <textarea class="form-control" [formControlName]="field.name"></textarea>
-              </div>
-              <div *ngSwitchCase="'email'">
-                <input class="form-control" [type]="field.type" [formControlName]="field.name">
-              </div>
-              <div *ngSwitchCase="'select'">
-                <select class="form-select" [formControlName]="field.name">
-                  <option *ngFor="let option of field.selectOptions" [ngValue]="option.value">{{ option.label }}</option>
-                </select>
-              </div>
-              <div *ngSwitchCase="'selectd'">
-              <select class="form-select" [formControlName]="field.name">
-                <ng-container *ngIf="isLoadingSelect; else options">
-                  <option disabled>Loading...</option>
-                </ng-container>
-                <ng-template #options>
-                  <option [ngValue]="null">Please Select an Option</option>
-                  <option *ngFor="let option of field.selectOptions$" [ngValue]="option.id">{{ option.name }}</option>
-                </ng-template>
-              </select>
-              </div>
-              <td *ngSwitchCase="'checkbox'" class="form-check form-switch">
-                <input type="checkbox" [formControlName]="field.name" [id]="field.name" class="form-check-input">
-              </td>
-            </ng-container>
-          </div>
+     <!-- <div *ngFor="let row of customLayout">
+        <div class="row g-3">
+           <ng-container *ngFor="let field of row">
+              <div class="col-md-{{ 12 / row.length }}"> -->
+                <div *ngFor="let field of formMetadata">
+                  <div class="form-group" *ngIf="field.type !== 'hidden'">
+                    <ng-container *ngIf="field.isTitle">
+                      <h5>{{ field.label }}</h5>
+                    </ng-container>
+                    <ng-container *ngIf="!field.isTitle">
+                      <div class="form-group">
+                        <div class="form-outline form-input-custom">
+                          <label [class.requiredak]="field.requiredasterisk" class="form-label" [for]="field.name">{{ field.label }}</label>
+                          <fa-icon
+                                placement="bottom"
+                                ngbTooltip="{{field.tooltip}}"
+                                container="body"
+                                [icon]="faInfoCircle"
+                                aria-hidden="true"
+                                class="gray-light-ico display-col"
+                                *ngIf="field.tooltip"
+                          ></fa-icon>
+                          <ng-container [ngSwitch]="field.type">
+                            <td *ngSwitchCase="'number'">
+                              <input class="form-control" type="number" [formControlName]="field.name">
+                            </td>
+                            <div *ngSwitchCase="'text'">
+                              <input class="form-control" [type]="field.type" [formControlName]="field.name">
+                            </div>
+                            <div *ngSwitchCase="'password'">
+                              <input class="form-control" type="password" [formControlName]="field.name">
+                            </div>
+                            <div *ngSwitchCase="'textarea'">
+                              <textarea class="form-control" [formControlName]="field.name"></textarea>
+                            </div>
+                            <div *ngSwitchCase="'email'">
+                              <input class="form-control" [type]="field.type" [formControlName]="field.name">
+                            </div>
+                            <div *ngSwitchCase="'select'">
+                              <select class="form-select" [formControlName]="field.name">
+                                <option *ngFor="let option of field.selectOptions" [ngValue]="option.value">{{ option.label }}</option>
+                              </select>
+                            </div>
+                            <div *ngSwitchCase="'selectd'">
+                            <select class="form-select" [formControlName]="field.name">
+                              <ng-container *ngIf="isLoadingSelect; else options">
+                                <option disabled>Loading...</option>
+                              </ng-container>
+                              <ng-template #options>
+                                <option [ngValue]="null">Please Select an Option</option>
+                                <option *ngFor="let option of field.selectOptions$" [ngValue]="option.id">{{ option.name }}</option>
+                              </ng-template>
+                            </select>
+                            </div>
+                            <td *ngSwitchCase="'checkbox'" class="form-check form-switch">
+                              <input type="checkbox" [formControlName]="field.name" [id]="field.name" class="form-check-input">
+                            </td>
+                          </ng-container>
+                        </div>
+                      </div>
+                    </ng-container>
+                  </div>
+                </div>
+              <!-- </div>
+           </ng-container>
         </div>
-      </ng-container>
-    </div>
-    </div>
-    <grid-buttons>
-      <button-submit name="Cancel" [disabled]="false" type="cancel" *ngIf="isCreateMode"></button-submit>
-      <button-submit name="Delete" [disabled]="false" type="delete" *ngIf="!isCreateMode && showDeleteButton" (click)="onDelete()">Delete</button-submit>
-      <button-submit [name]="buttonText" [disabled]="!formIsValid()"></button-submit>
-    </grid-buttons>
+     </div> -->
+     <grid-buttons>
+        <button-submit name="Cancel" [disabled]="false" type="cancel" *ngIf="isCreateMode"></button-submit>
+        <button-submit name="Delete" [disabled]="false" type="delete" *ngIf="!isCreateMode && showDeleteButton" (click)="onDelete()">Delete</button-submit>
+        <button-submit [name]="buttonText" [disabled]="!formIsValid()"></button-submit>
+      </grid-buttons>
   </form>
 </grid-main>
   `,
@@ -115,6 +123,11 @@ export class DynamicFormComponent implements OnInit, AfterViewInit, OnDestroy {
    * The Angular FormGroup that represents the dynamic form.
    */
   @Input() form: FormGroup;
+
+  /**
+   * Define the custom layout array.
+   */
+  @Input() customLayout: any[];
 
   /**
    * Indicates whether the form is in "create" mode or "update" mode.
@@ -185,9 +198,9 @@ export class DynamicFormComponent implements OnInit, AfterViewInit, OnDestroy {
         if (field.type === 'checkbox') {
           // For checkboxes, use the value directly from formValues.
           initialValue = this.formValues[fieldName];
-        } else {
+        } if(!this.isCreateMode) {
           // For other field types, use formValues[fieldName] or 0 as a default value if not provided.
-          initialValue = fieldName in this.formValues ? this.formValues[fieldName] : 0;
+          initialValue =  fieldName in this.formValues ? (this.formValues[fieldName] === 0 ? 0 : this.formValues[fieldName]) : 0;
         }
 
         // In 'create' mode, override the initial value if a default value is specified in the field's metadata.
